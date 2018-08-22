1ea637b0ed1210c7d2f6373c821ac9a321cc7b82
fsegall <segall.felipe@gmail.com>
2018-08-22 17:57:44 -0300

    Start filtering by name

--- src/App.js
+++ src/App.js
@@ -5,36 +5,71 @@ import Eleicao from "./dadosPresidente.json";
 localStorage.setItem("eleicao", JSON.stringify(Eleicao.cand));
 
 // How to sort an array of objects by numerical value
-const itensEleitos = JSON.parse(localStorage.getItem("eleicao")).sort(function(
+const itensEleitos = JSON.parse(localStorage.getItem("eleicao")).sort(function (
   obj1,
   obj2
 ) {
   return obj1.seq - obj2.seq;
 });
 
-const candidato = itensEleitos.map(eleito => {
-  const classEleito =
-    eleito.e === "s"
-      ? "list-group-item list-group-item-action list-group-item-primary"
-      : "list-group-item list-group-item-action list-group-item-danger";
+class Candidato extends Component {
+  render() {
 
-  return (
-    <li className={classEleito} key={eleito.sqcand}>
-      {eleito.e === "s"
-        ? `O candidato ${eleito.nm} foi eleito com ${
+    const items = this.props.itens.map(eleito => {
+      const classEleito =
+        eleito.e === "s"
+          ? "list-group-item list-group-item-action list-group-item-primary"
+          : "list-group-item list-group-item-action list-group-item-danger";
+
+      return (
+
+        <li className={classEleito} key={eleito.sqcand}>
+          {eleito.e === "s"
+            ? `O candidato ${eleito.nm} foi eleito com ${
             eleito.v
-          } votos e ficou em ${eleito.seq}º lugar.`
-        : `O candidato ${eleito.nm} não foi eleito com ${
+            } votos e ficou em ${eleito.seq}º lugar.`
+            : `O candidato ${eleito.nm} não foi eleito com ${
             eleito.v
-          } votos e ficou em ${eleito.seq}º lugar.`}
-    </li>
-  );
-});
+            } votos e ficou em ${eleito.seq}º lugar.`}
+        </li>
+
+      )
+    });
+
+    return items
+
+  }
+
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      itensEleitos
+    }
+    this.onchange = this.onChange.bind(this);
+  }
+
+  onChange = (e) => {
+    let updatedList = this.state.itensEleitos.filter(item => item.nm.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+    )
+
+    this.setState({
+      itensEleitos: updatedList
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <label>
+          Filtrar por nome:
+          <input
+            type="text"
+            onChange={this.onChange}
+          />
+        </label>
         <h1
           style={{
             color: "tomato",
@@ -44,7 +79,9 @@ class App extends Component {
         >
           Resultados das Eleições 2018
         </h1>
-        <ul className="list-group">{candidato}</ul>
+        <ul className="list-group">
+          <Candidato itens={this.state.itensEleitos} />
+        </ul>
       </div>
     );
   }
